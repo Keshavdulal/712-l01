170ef3589505100d559df7461b4e9e02b60edb85
keshavdulal <keshav.dulal@gmail.com>
2021-09-16 00:42:27 +0545

    add missing files

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => Node = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Keshav Dulal</Text>
+        <Text>301209947</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
