9142dedee6ca3e42a46fa8192873f7803a2fcdd2
keshavdulal <keshav.dulal@gmail.com>
2021-09-16 05:05:30 +0545

    center text info & code cleanup

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+// import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => Node = () => {
+const App = () => {
   return (
-    <SafeAreaView>
-      <View>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <SafeAreaView>
         <Text>Keshav Dulal</Text>
         <Text>301209947</Text>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </View>
   );
 };
 
